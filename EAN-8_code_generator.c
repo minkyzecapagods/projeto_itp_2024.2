92d5fa60647d2be5de72bc2fe803069c6bef3c50
Arthur Silva <tuzaum.silva@gmail.com>
2024-12-31 17:49:55 -0300

    Separando arquivos e adicionando instruções

---
+++ EAN-8_code_generator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+        // 1. Receber um código de 8 dígitos e verificar se ele é válido;
+        // 2. Se for válido, converter o código de 8 dígitos para código EAN-8;
+        // 3. A partir do código EAN-8, criar uma imagem PBM
+
+        return 0;
+}
