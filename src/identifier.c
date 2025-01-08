e040a276fb916738cb49633862ee6cc2efe7cad4
Arthur Silva <tuzaum.silva@gmail.com>
2025-01-08 15:06:01 -0300

    Fiz the identifier function

--- src/identifier.c
+++ src/identifier.c
@@ -1,49 +1,47 @@
-//
-// Created by tuzin on 1/7/25.
-//
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-char *int_to_str(int n) {
-        // Aloca memória para armazenar a string
-        char *str = (char *)malloc(12 * sizeof(char));  // 12 é suficiente para armazenar até um inteiro de 32 bits
-        if (str == NULL) {
-                printf("Erro de alocação de memória\n");
-                return NULL;
+void traduzirEAN8(int identificador) {
+        char lcodes[10][8] = {
+                "0001101", "0011001", "0010011", "0111101", "0100011",
+                "0110001", "0101111", "0111011", "0110111", "0001011"
+            };
+        char rcodes[10][8] = {
+                "1110010", "1100110", "1101100", "1000010", "1011100",
+                "1001110", "1010000", "1000100", "1001000", "1110100"
+            };
+
+        // Código de início do EAN-8
+        char codigo[100] = "101";
+
+        // Processa os primeiros 4 dígitos com L-code
+        for (int i = 7; i >= 4; i--) {
+                int digito = (identificador / (int)pow(10, i)) % 10;
+                strcat(codigo, lcodes[digito]);
         }
-        // Converte o número em string
-        sprintf(str, "%d", n);
-        return str;
+
+        // Adiciona o código do meio
+        strcat(codigo, "01010");
+
+        // Processa os últimos 4 dígitos com R-code
+        for (int i = 3; i >= 0; i--) {
+                int digito = (identificador / (int)pow(10, i)) % 10;
+                strcat(codigo, rcodes[digito]);
+        }
+
+        // Adiciona o código de fim
+        strcat(codigo, "101");
+
+        // Imprime o código de barras traduzido
+        printf("Código de Barras EAN-8: %s\n", codigo);
 }
 
-
 int main() {
-        int lcodes[10][1] = {
-                {0001101}, {0011001}, {0010011}, {0111101}, {0100011},
-                {0110001}, {0101111}, {0111011}, {0110111}, {0001011}
-        };
-        int rcodes[10][1] = {
-                {1110010}, {1100110}, {1101100}, {1000010}, {1011100},
-                {1001110}, {1010000}, {1000100}, {1001000}, {1110100}
-        };
-        int identifier;
+        int identificador;
+        printf("Digite um identificador de 8 dígitos: ");
+        scanf("%d", &identificador);
 
-        char barcode[68] = "101";
-
-        scanf("%d", &identifier);
-        char *str = int_to_str(identifier);
-        for (int i = 0; i < 4; i++) {
-                strcat(barcode, int_to_str(lcodes[atoi(str[i])][0]));
-        }
-        for (int i = 4; i < 8; i++) {
-                strcat(barcode, int_to_str(rcodes[atoi(str[i])][0]));
-        }
-        printf("%s\n", barcode);
-        free(str);
-
-
-
+        traduzirEAN8(identificador);
         return 0;
 }
